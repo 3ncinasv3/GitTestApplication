9be972d6c03d0a9f902672db3bbc1bb7f05acb7a
Joshua Encinas Valador <encinasv@sheridancollege.ca>
2023-05-21 16:29:52 -0400

    added address attribute

---
+++ src/gittestapplication/GitTestApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gittestapplication;
+
+/**
+ *
+ * @author jv
+ */
+public class GitTestApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
